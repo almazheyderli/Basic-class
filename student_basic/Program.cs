d9d9aa841c7b668ffa5ae31a47eec3d89b57536a
almazheyderli <bd7g7vr18@code.edu.az>
2024-04-05 08:31:06 +0400

    class basic

---
+++ student_basic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace student_basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
